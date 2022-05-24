26ea153a49ba7656ed1c0951ea67f96ce00a501b
Michael Patsko <michael_patsko@yahoo.co.uk>
2022-05-24 14:31:28 +0100

    remove changes to App.tsx and create FontAwesomeIcon in Home.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
 import './App.scss'
 import { Home } from "./components/Home";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faStar, faRocket, faEarthAmericas, faUserAstronaut } from "@fortawesome/free-solid-svg-icons";
 
 function App() {
   return (
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,6 +1,10 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faStar, faRocket, faEarthAmericas, faUserAstronaut } from "@fortawesome/free-solid-svg-icons";
+
 export function Home() {
     return <main>
         <h1>Welcome to Mars Mission!</h1>
         <p>Welcome, dear guest, to Mars Mission!</p>
+        <FontAwesomeIcon icon={faStar} size="6x" spin/>
     </main>
 }
