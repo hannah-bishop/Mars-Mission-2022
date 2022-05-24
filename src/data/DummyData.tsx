ec44650441cee467dbc0d1d64f31a83d41dcf70a
jordan-renaud <jordan.dj.renaud@gmail.com>
2022-05-24 15:50:24 +0100

    MM-24: Add dummy data file

---
+++ src/data/DummyData.tsx
@@ -0,0 +1,92 @@
+export const learningPageData = [
+  {
+    title: "Section 1",
+    content: {
+      imageUrl: "https://picsum.photos/200/300",
+      text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Integer quis auctor elit sed vulputate mi sit amet. Est ultricies integer quis auctor. Laoreet id donec ultrices tincidunt arcu. Vel facilisis volutpat est velit egestas dui id.",
+    },
+    questionSection: {
+      question: "Question 1",
+      multipleChoices: ["a", "b", "c", "d"],
+      correctAnswer: "a",
+    },
+  },
+  {
+    title: "Section 2",
+    content: {
+      imageUrl: "https://picsum.photos/200/300",
+      text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Integer quis auctor elit sed vulputate mi sit amet. Est ultricies integer quis auctor. Laoreet id donec ultrices tincidunt arcu. Vel facilisis volutpat est velit egestas dui id.",
+    },
+    questionSection: {
+      question: "Question 2",
+      multipleChoices: ["a", "b", "c", "d"],
+      correctAnswer: "b",
+    },
+  },
+  {
+    title: "Section 3",
+    content: {
+      imageUrl: "https://picsum.photos/200/300",
+      text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Integer quis auctor elit sed vulputate mi sit amet. Est ultricies integer quis auctor. Laoreet id donec ultrices tincidunt arcu. Vel facilisis volutpat est velit egestas dui id.",
+    },
+    questionSection: {
+      question: "Question 3",
+      multipleChoices: ["a", "b", "c", "d"],
+      correctAnswer: "c",
+    },
+  },
+  {
+    title: "Section 4",
+    content: {
+      imageUrl: "https://picsum.photos/200/300",
+      text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Integer quis auctor elit sed vulputate mi sit amet. Est ultricies integer quis auctor. Laoreet id donec ultrices tincidunt arcu. Vel facilisis volutpat est velit egestas dui id.",
+    },
+    questionSection: {
+      question: "Question 4",
+      multipleChoices: ["a", "b", "c", "d"],
+      correctAnswer: "d",
+    },
+  },
+  {
+    title: "Section 5",
+    content: {
+      imageUrl: "https://picsum.photos/200/300",
+      text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Integer quis auctor elit sed vulputate mi sit amet. Est ultricies integer quis auctor. Laoreet id donec ultrices tincidunt arcu. Vel facilisis volutpat est velit egestas dui id.",
+    },
+    questionSection: {
+      question: "Question 5",
+      multipleChoices: ["a", "b", "c", "d"],
+      correctAnswer: "a",
+    },
+  },
+];
+
+export const marsHolidayData = {
+  defaultOptions: {
+    weatherData: { temp: 20, description: "Cloudy" },
+    durationOfFlightInMinutes: 900,
+    costInPounds: "£100,000",
+  },
+  questionSection: [
+    {
+      question: "Question 1",
+      multipleChoices: ["a", "b", "c", "d"],
+    },
+    {
+      question: "Question 2",
+      multipleChoices: ["a", "b", "c", "d"],
+    },
+    {
+      question: "Question 3",
+      multipleChoices: ["a", "b", "c", "d"],
+    },
+    {
+      question: "Question 4",
+      multipleChoices: ["a", "b", "c", "d"],
+    },
+    {
+      question: "Question 5",
+      multipleChoices: ["a", "b", "c", "d"],
+    },
+  ],
+};
