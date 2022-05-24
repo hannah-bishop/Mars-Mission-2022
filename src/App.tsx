4eebc80ac6f793c73f6c18ff5cc25fedb130b8e1
Michael Patsko <michael_patsko@yahoo.co.uk>
2022-05-24 12:31:31 +0100

    MM-22: Install npm packages and import icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import './App.scss'
 import { Home } from "./components/Home";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faStar, faRocket, faEarthAmericas, faUserAstronaut } from "@fortawesome/free-solid-svg-icons";
 
 function App() {
   return (
-    <Home />
-  )
+    <Home />)
 }
 
 export default App
