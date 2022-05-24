a29aff93242a3f9005a16de5ceaf275eacd04b9b
hbishop1996@gmail.com <Hannah.Bishop@softwire.com>
2022-05-24 15:32:15 +0100

    MM-03: Add suggested ternary operator

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -4,31 +4,31 @@ import React, { useState } from "react";
 export function Menu() {
   const [isOpen, setIsOpen] = useState(false);
 
-  if (isOpen) {
-    return (
-      <nav onClick={() => setIsOpen(false)}>
-        <button>Menu</button>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/learning-space">Learning Space</Link>
-          </li>
-          <li>
-            <Link to="/mars-holiday">Mars Holiday</Link>
-          </li>
-          <li>
-            <Link to="/images">Mars Rover Photos</Link>
-          </li>
-        </ul>
-      </nav>
-    );
-  } else {
-    return (
-      <button className="menu-button" onClick={() => setIsOpen(true)}>
-        Menu
-      </button>
-    );
-  }
+  return (
+    <nav>
+      {isOpen ? (
+        <div onClick={() => setIsOpen(false)}>
+          <button>Menu</button>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/learning-space">Learning Space</Link>
+            </li>
+            <li>
+              <Link to="/mars-holiday">Mars Holiday</Link>
+            </li>
+            <li>
+              <Link to="/images">Mars Rover Photos</Link>
+            </li>
+          </ul>
+        </div>
+      ) : (
+        <button className="menu-button" onClick={() => setIsOpen(true)}>
+          Menu
+        </button>
+      )}
+    </nav>
+  );
 }
