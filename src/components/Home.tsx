95430e3ca2e1c7c342ad10836365ee10a102b186
Palak Mittal <Palak.Mittal@softwire.com>
2022-05-25 10:55:35 +0100

    create grid elements

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -8,7 +8,12 @@ export function Home() {
       <h1>Welcome to Mars Mission!</h1>
       <p>Welcome, dear guest, to Mars Mission!</p>
       <img src="https://www.solarsystemscope.com/spacepedia/images/handbook/renders/mars.png" />
-      <FontAwesomeIcon icon={faStar} size="6x" spin/>
+      <FontAwesomeIcon icon={faStar} size="6x" spin />
+      <div className="navigation-grid">
+        <div className="learning-space-tile"> Learning Space </div>
+        <div className="mars-holiday-tile"> Mars Holiday </div>
+        <div className="photos-rover-tile"> Mars Rover Photos</div>
+      </div>
     </main>
   );
 }
