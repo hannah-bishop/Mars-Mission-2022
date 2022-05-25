fbe7c0e6f494bdce4af6ad14433810eff547cfa5
Palak Mittal <Palak.Mittal@softwire.com>
2022-05-25 12:04:41 +0100

    grid elements with icons complete

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,6 +1,11 @@
 import "./Home.scss";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faStar } from "@fortawesome/free-solid-svg-icons";
+import {
+  faStar,
+  faUserAstronaut,
+  faRocket,
+} from "@fortawesome/free-solid-svg-icons";
 
 export function Home() {
   return (
@@ -8,11 +13,27 @@ export function Home() {
       <h1>Welcome to Mars Mission!</h1>
       <p>Welcome, dear guest, to Mars Mission!</p>
       <img src="https://www.solarsystemscope.com/spacepedia/images/handbook/renders/mars.png" />
-      <FontAwesomeIcon icon={faStar} size="6x" spin />
       <div className="navigation-grid">
-        <div className="learning-space-tile"> Learning Space </div>
-        <div className="mars-holiday-tile"> Mars Holiday </div>
-        <div className="photos-rover-tile"> Mars Rover Photos</div>
+        <Link to="/learning-space">
+          {" "}
+          <div className="learning-space-tile">
+            <FontAwesomeIcon icon={faUserAstronaut} />
+            Learning Space
+          </div>
+        </Link>
+        <Link to="/mars-holiday">
+          {" "}
+          <div className="mars-holiday-tile">
+            <FontAwesomeIcon icon={faRocket} />
+            Mars Holiday
+          </div>
+        </Link>
+        <Link to="/images" className="photos-rover-tile">
+          <div>
+            <FontAwesomeIcon icon={faStar} />
+            Mars Rover Photos
+          </div>
+        </Link>
       </div>
     </main>
   );
