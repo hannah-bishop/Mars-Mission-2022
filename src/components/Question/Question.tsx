de7fbb3f44df7cd16929c49099bfe65452f86058
hbishop1996@gmail.com <Hannah.Bishop@softwire.com>
2022-05-26 11:18:39 +0100

    MM-02: Add functionality to question component

--- src/components/Question/Question.tsx
+++ src/components/Question/Question.tsx
@@ -1,7 +1,45 @@
-export function Question(questionSection:object){
-    return(
-        <div>
-            {questionSection.questionText}
-        </div>
-    )
+import { QuestionSection, Answer } from "../../data/dummyData";
+import { useState } from "react";
+import "./Question.scss";
+
+interface QuestionProps {
+  section: QuestionSection;
+  onAnswerChosen: (arg0: Answer) => void;
+}
+
+export function Question(props: QuestionProps) {
+  const question = props.section.questionText;
+  const answers = props.section.answers;
+  const [isAnswered, setIsAnswered] = useState(false);
+  const [chosenAnswer, setChosenAnswer] = useState<Answer>();
+  return (
+    <div>
+      {question}
+      <ol>
+        {answers.map((answer) => (
+          <li
+            onClick={() => {
+              if (isAnswered) {
+                return;
+              }
+              setIsAnswered(true);
+              setChosenAnswer(answer);
+              props.onAnswerChosen(answer);
+            }}
+            className={`${
+              !isAnswered
+                ? ""
+                : answer.isCorrect
+                ? "correct-answer"
+                : chosenAnswer?.text === answer.text
+                ? "wrong-answer"
+                : ""
+            }`}
+          >
+            {answer.text}
+          </li>
+        ))}
+      </ol>
+    </div>
+  );
 }
