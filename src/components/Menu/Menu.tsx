3a09136b5f883ce5c528c9df9be1f9100a42a367
Michael Patsko <michael_patsko@yahoo.co.uk>
2022-05-26 11:22:46 +0100

    added margins and padding of menu options

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -13,17 +13,17 @@ export function Menu() {
         <div onClick={() => setIsOpen(false)}  className="menu-screen">
           <button>Menu</button>
           <ul>
-            <li>
-              <Link to="/"  className="menu-list-item"><FontAwesomeIcon icon={faEarthAmericas} size="lg"/> Home</Link>
+            <li className="menu-list-item">
+              <Link to="/"  className="menu-link"><FontAwesomeIcon icon={faEarthAmericas} size="lg"/> Home</Link>
             </li>
-            <li>
-              <Link to="/learning-space"  className="menu-list-item"><FontAwesomeIcon icon={faUserAstronaut} size="lg"/> Learning Space</Link>
+            <li className="menu-list-item">
+              <Link to="/learning-space"  className="menu-link"><FontAwesomeIcon icon={faUserAstronaut} size="lg"/> Learning Space</Link>
             </li>
-            <li>
-              <Link to="/mars-holiday"  className="menu-list-item"><FontAwesomeIcon icon={faRocket} size="lg"/> Mars Holiday</Link>
+            <li className="menu-list-item">
+              <Link to="/mars-holiday"  className="menu-link"><FontAwesomeIcon icon={faRocket} size="lg"/> Mars Holiday</Link>
             </li>
-            <li>
-              <Link to="/images"  className="menu-list-item"><FontAwesomeIcon icon={faStar} size="lg"/> Mars Rover Photos</Link>
+            <li className="menu-list-item">
+              <Link to="/images"  className="menu-link"><FontAwesomeIcon icon={faStar} size="lg"/> Mars Rover Photos</Link>
             </li>
           </ul>
         </div>
