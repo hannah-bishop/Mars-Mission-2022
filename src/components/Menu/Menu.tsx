5f6967eeb079f2195a9143d05529a9a5c9b18676
Michael Patsko <michael_patsko@yahoo.co.uk>
2022-05-26 11:09:57 +0100

    created dropdown menu styling

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faStar, faEarthAmericas, faUserAstronaut, faRocket } from "@fortawesome/free-solid-svg-icons";
+import "./Menu.scss";
 
 export function Menu() {
   const [isOpen, setIsOpen] = useState(false);
@@ -9,20 +10,20 @@ export function Menu() {
   return (
     <nav>
       {isOpen ? (
-        <div onClick={() => setIsOpen(false)}>
+        <div onClick={() => setIsOpen(false)}  className="menu-screen">
           <button>Menu</button>
           <ul>
             <li>
-              <Link to="/"><FontAwesomeIcon icon={faEarthAmericas} size="lg"/> Home</Link>
+              <Link to="/"  className="menu-list-item"><FontAwesomeIcon icon={faEarthAmericas} size="lg"/> Home</Link>
             </li>
             <li>
-              <Link to="/learning-space"><FontAwesomeIcon icon={faUserAstronaut} size="lg"/> Learning Space</Link>
+              <Link to="/learning-space"  className="menu-list-item"><FontAwesomeIcon icon={faUserAstronaut} size="lg"/> Learning Space</Link>
             </li>
             <li>
-              <Link to="/mars-holiday"><FontAwesomeIcon icon={faRocket} size="lg"/> Mars Holiday</Link>
+              <Link to="/mars-holiday"  className="menu-list-item"><FontAwesomeIcon icon={faRocket} size="lg"/> Mars Holiday</Link>
             </li>
             <li>
-              <Link to="/images"><FontAwesomeIcon icon={faStar} size="lg"/> Mars Rover Photos</Link>
+              <Link to="/images"  className="menu-list-item"><FontAwesomeIcon icon={faStar} size="lg"/> Mars Rover Photos</Link>
             </li>
           </ul>
         </div>
