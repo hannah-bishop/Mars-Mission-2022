48e6d33488c076fa7254de6f67126166af805bdd
Michael Patsko <michael_patsko@yahoo.co.uk>
2022-05-26 14:00:29 +0100

    added styling for menu button

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -1,46 +1,76 @@
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faStar, faEarthAmericas, faUserAstronaut, faRocket } from "@fortawesome/free-solid-svg-icons";
+import {
+  faStar,
+  faEarthAmericas,
+  faUserAstronaut,
+  faRocket,
+  faBars,
+} from "@fortawesome/free-solid-svg-icons";
 import "./Menu.scss";
 
 export function Menu() {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <nav>
+    <nav className="dropdown-menu">
       {isOpen ? (
-        <div onClick={() => setIsOpen(false)}  className="menu-screen">
-          <button>Menu</button>
+        <div onClick={() => setIsOpen(false)} className="menu-screen">
+          <FontAwesomeIcon icon={faBars} className="menu-button" size="2x" />
           <ul>
-            <Link to="/">
+            <Link to="/" className="menu-link">
               <li className="menu-list-item">
-                <p className="menu-link"><FontAwesomeIcon icon={faEarthAmericas} size="lg"/> Home</p>
+                <FontAwesomeIcon
+                  icon={faEarthAmericas}
+                  size="3x"
+                  className="menu-icon"
+                />
+                <div className="menu-link-text">Home</div>
               </li>
             </Link>
 
-            <Link to="/learning-space">
+            <Link to="/learning-space" className="menu-link">
               <li className="menu-list-item">
-                <p className="menu-link"><FontAwesomeIcon icon={faUserAstronaut} size="lg"/> Learning Space</p>
-              </li>
-            </Link>
-            <Link to="/mars-holiday">
-              <li className="menu-list-item">
-                <p className="menu-link"><FontAwesomeIcon icon={faRocket} size="lg"/> Mars Holiday</p>
-              </li>
-            </Link>
-            <Link to="/images">
-              <li className="menu-list-item">
-                <p className="menu-link"><FontAwesomeIcon icon={faStar} size="lg"/> Mars Rover Photos</p>
+                <FontAwesomeIcon
+                  icon={faUserAstronaut}
+                  size="3x"
+                  className="menu-icon"
+                />
+                <div className="menu-link-text">Learning Space</div>
               </li>
             </Link>
 
+            <Link to="/mars-holiday" className="menu-link">
+              <li className="menu-list-item">
+                <FontAwesomeIcon
+                  icon={faRocket}
+                  size="3x"
+                  className="menu-icon"
+                />
+                <div className="menu-link-text">Mars Holiday</div>
+              </li>
+            </Link>
+
+            <Link to="/images" className="menu-link">
+              <li className="menu-list-item">
+                <FontAwesomeIcon
+                  icon={faStar}
+                  size="3x"
+                  className="menu-icon"
+                />
+                <div className="menu-link-text">Mars Rover Photos</div>
+              </li>
+            </Link>
           </ul>
         </div>
       ) : (
-        <button className="menu-button" onClick={() => setIsOpen(true)}>
-          Menu
-        </button>
+        <FontAwesomeIcon
+          icon={faBars}
+          className="menu-button"
+          onClick={() => setIsOpen(true)}
+          size="2x"
+        />
       )}
     </nav>
   );
