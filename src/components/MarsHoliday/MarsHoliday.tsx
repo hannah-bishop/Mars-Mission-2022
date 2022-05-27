b4edf8c700e532c9c91a257227abeb9fb11409aa
Patricia Robles <patriciarobles9@gmail.com>
2022-05-27 12:07:28 +0100

    MM-07: Create styling and form for MarsHoliday

--- src/components/MarsHoliday/MarsHoliday.tsx
+++ src/components/MarsHoliday/MarsHoliday.tsx
@@ -1,8 +1,11 @@
+import { TicketComponent } from "../TicketComponent/TicketComponent";
+import "./MarsHoliday.scss";
 
 export function MarsHoliday() {
   return (
     <div>
-      <h1>Would you dare to go to Mars?</h1>
+      <h1 className="header">Would you dare to go to Mars?</h1>
+      <TicketComponent />
     </div>
   );
 }
---
+++ src/components/TicketComponent/TicketComponent.tsx
@@ -0,0 +1,35 @@
+import ReactDOM from "react-dom/client";
+import React from "react";
+import "./TicketComponent.scss";
+
+export function TicketComponent() {
+  return (
+    <div>
+      //{" "}
+      <img
+        className="image-mars-holiday"
+        src="https://images-ext-2.discordapp.net/external/X8VyhfWYjxx4TkSvBCl4zPxmZ8JEw_H8WyzsaaTLRLg/https/www.solarsystemscope.com/spacepedia/images/handbook/renders/mars.png"
+      />
+      <h2> Your Ticket to Mars</h2>
+      <form className="grid-container">
+        <label>
+          {" "}
+          Your name:
+          <input type="text" />
+        </label>
+        <label>
+          {" "}
+          Your surname:
+          <input type="text" />
+        </label>
+        <label>
+          {" "}
+          Date:
+          <input type="text" />
+        </label>
+      </form>
+    </div>
+  );
+}
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(<TicketComponent />);
