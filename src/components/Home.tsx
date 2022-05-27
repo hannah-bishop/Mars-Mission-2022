76523f095dea511af5e66692dc1a878b52094e0c
Michael Patsko <michael_patsko@yahoo.co.uk>
2022-05-27 10:33:21 +0100

    add border styling to navigation grid

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -17,17 +17,20 @@ export function Home() {
       <div className="navigation-grid">
         <Link to="/learning-space" className="grid-link">
           <div className="grid-text">
-            <FontAwesomeIcon icon={faUserAstronaut} /> Learning Space
+            <FontAwesomeIcon icon={faUserAstronaut} className="grid-icon" />{" "}
+            Learning Space
           </div>
         </Link>
         <Link to="/mars-holiday" className="grid-link">
           <div className="grid-text">
-            <FontAwesomeIcon icon={faRocket} /> Mars Holiday
+            <FontAwesomeIcon icon={faRocket} className="grid-icon" /> Mars
+            Holiday
           </div>
         </Link>
         <Link to="/images" className="grid-span grid-link">
           <div className="grid-text">
-            <FontAwesomeIcon icon={faStar} /> Mars Rover Photos
+            <FontAwesomeIcon icon={faStar} className="grid-icon" /> Mars Rover
+            Photos
           </div>
         </Link>
       </div>
